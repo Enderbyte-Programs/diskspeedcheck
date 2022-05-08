e40aa2836217ff45d4fc63d9b2729b1f6cb7552e
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2022-05-08 08:59:58 -0700

    v1.0.0

--- diskspeedcheck/diskspeedcheck.py
+++ diskspeedcheck/diskspeedcheck.py
@@ -12,7 +12,8 @@ def silent_exc(type,value,traceback):
     sys.exit(1)
 
 #Package stuff
-__version__ = "10"
+__version__ = "1.0.0"
+__doc__ = """Diskspeedcheck is a small utility that checks your disk's write speed. It is highly customizeable."""
 
 #Getting arguments and removing filename
 args = sys.argv
@@ -29,6 +30,22 @@ def pverb(data):
     if uv:
         print(millis(),end=" ")
         print(data)
+
+def average(ltba,errors="halt"):
+    """Averages a list. errors halt means it will throw a typeerror. skip means it will skip over it and not throw an error"""
+    t = 0
+    success = 0
+    for item in ltba:
+        try:
+            t += item
+        except TypeError:
+            if errors.lower()=="halt":
+                raise TypeError("Invalid item in list!")
+        else:
+            success += 1
+    avg = t / success
+    return avg
+
 if "-s" in args or "--silent" in args:
     us = False
     sys.excepthook = silent_exc
@@ -106,44 +123,101 @@ if os.path.isfile(assembledname) and not "--surpressfile" in args:
     sys.exit(4)
 pverb("Writing file")
 
-
-if "--excludefmk" in args or "-e" in args:
-    pverb("Excluding file make")
+if "-a" in args:
     try:
-        with open(assembledname,"w+") as f:
+        aloop = int(args[args.index("-a")+1])
+    except IndexError:
+        pverb("No average value provided, going with 10")
+        aloop = 10
+    except ValueError:
+        pverb("Invalid average value provided, going with 10")
+        aloop = 10
+    speeds = []
+    dspeeds = []
+    for i in range(aloop):
+        pverb(f"Running test {i+1}/{aloop}")
+        if os.path.isfile(assembledname) and not "--surpressfile" in args:
+            os.remove(assembledname)
+        if "--excludefmk" in args or "-e" in args:
+            pverb("Excluding file make")
+            try:
+                with open(assembledname,"w+") as f:
+                    start = datetime.now()
+                    f.write(writeable)
+                    end = datetime.now()
+            except PermissionError:
+                if us:
+                    print("ERROR Permission denied")
+                sys.exit(2)
+        else:
             start = datetime.now()
-            f.write(writeable)
-            end = datetime.now()
-    except PermissionError:
-        if us:
-            print("ERROR Permission denied")
-        sys.exit(2)
-else:
-    start = datetime.now()
-    try:
-        with open(assembledname,"w+") as f:
-            f.write(writeable)           
-    except PermissionError:
-        if us:
-            print("ERROR Permission denied")
-        sys.exit(2)
-    end = datetime.now()
+            try:
+                with open(assembledname,"w+") as f:
+                    f.write(writeable)           
+            except PermissionError:
+                if us:
+                    print("ERROR Permission denied")
+                sys.exit(2)
+            end = datetime.now() 
+        speed = end - start
+        speed = speed.total_seconds()*1000
+        dspeed = 1000 / speed
+        dspeed = dspeed / diffval
+        speeds.append(speed)
+        dspeeds.append(dspeed)
+        if "--noround" in args:
+            pverb(f"Speed is {dspeed} MB/s")
+            pverb(f"Wrote {wsize/1000} KB to the disk in {speed} milliseconds.")
+        else:
+            pverb(f"Speed is {round(dspeed,3)} MB/s")
+            pverb(f"Wrote {wsize/1000} KB to the disk in {round(speed,3)} milliseconds.")
+    pverb("All tests done")
+    print(f"Average speed is {round(average(dspeeds),3)} Mb/s")
+    print(f"Average time is {round(average(speeds),3)} ms")
+    print(f"Lowest time was {min(speeds)} ms ({max(dspeeds)} Mb/s)")
+    print(f"Slowest time was {max(speeds)} ms ({min(dspeeds)} Mb/s)")
+    if not "--noremove" in args:
+        pverb("Removing file")
+        os.remove(assembledname)
 
-if not "--noremove" in args:
-    pverb("Removing file")
-    os.remove(assembledname)
-pverb("Calculating")
-speed = end - start
-speed = speed.total_seconds()*1000
-dspeed = 1000 / speed
-dspeed = dspeed / diffval
-pverb("Printing results")
-
-if "--noround" in args:
-    print(f"Speed is {dspeed} MB/s")
-    print(f"Wrote {wsize/1000} KB to the disk in {speed} milliseconds.")
 else:
-    print(f"Speed is {round(dspeed,3)} MB/s")
-    print(f"Wrote {wsize/1000} KB to the disk in {round(speed,3)} milliseconds.")
+    if "--excludefmk" in args or "-e" in args:
+        pverb("Excluding file make")
+        try:
+            with open(assembledname,"w+") as f:
+                start = datetime.now()
+                f.write(writeable)
+                end = datetime.now()
+        except PermissionError:
+            if us:
+                print("ERROR Permission denied")
+            sys.exit(2)
+    else:
+        start = datetime.now()
+        try:
+            with open(assembledname,"w+") as f:
+                f.write(writeable)           
+        except PermissionError:
+            if us:
+                print("ERROR Permission denied")
+            sys.exit(2)
+        end = datetime.now()
+
+    if not "--noremove" in args:
+        pverb("Removing file")
+        os.remove(assembledname)
+    pverb("Calculating")
+    speed = end - start
+    speed = speed.total_seconds()*1000
+    dspeed = 1000 / speed
+    dspeed = dspeed / diffval
+    pverb("Printing results")
+
+    if "--noround" in args:
+        print(f"Speed is {dspeed} MB/s")
+        print(f"Wrote {wsize/1000} KB to the disk in {speed} milliseconds.")
+    else:
+        print(f"Speed is {round(dspeed,3)} MB/s")
+        print(f"Wrote {wsize/1000} KB to the disk in {round(speed,3)} milliseconds.")
 pverb("Done")
 sys.exit(0)
