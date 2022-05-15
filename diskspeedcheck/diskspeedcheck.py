06e2cb9b27096c6ff6737e07eaaa383329664263
Enderbyte09 <83879635+Enderbyte09@users.noreply.github.com>
2022-05-15 11:15:08 -0700

    1.0.3
    See changelog

--- diskspeedcheck/diskspeedcheck.py
+++ diskspeedcheck/diskspeedcheck.py
@@ -6,7 +6,7 @@ import logging
 def silent_exc(type,value,traceback):
     sys.exit(1)
 #Package stuff
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 #Getting arguments and removing filename
 args = sys.argv
 del args[0]
@@ -36,7 +36,7 @@ if "-s" in args or "--silent" in args:
     logging.basicConfig(level=logging.CRITICAL,format="%(relativeCreated)s %(levelname)s %(message)s")
 else:
     us = True
-    logging.basicConfig(level=logging.ERROR,format="%(relativeCreated)s %(levelname)s %(message)s")
+    logging.basicConfig(level=logging.WARNING,format="%(relativeCreated)s %(levelname)s %(message)s")
 if uv and not us:
     raise RuntimeError("verbose and silent may not be used together!")
 #Defining variables
@@ -110,83 +110,97 @@ if "-a" in args:
     speeds = []
     dspeeds = []
     for i in range(aloop):
-        logging.info(f"Running test {i+1}/{aloop}")
-        if os.path.isfile(assembledname) and not "--surpressfile" in args:
-            os.remove(assembledname)
-        if "--excludefmk" in args or "-e" in args:
-            logging.info("Excluding file make")
+        success = False
+        while not success:
             try:
-                with open(assembledname,"w+") as f:
+                logging.info(f"Running test {i+1}/{aloop}")
+                if os.path.isfile(assembledname) and not "--surpressfile" in args:
+                    os.remove(assembledname)
+                if "--excludefmk" in args or "-e" in args:
+                    logging.info("Excluding file make")
+                    try:
+                        with open(assembledname,"w+") as f:
+                            start = datetime.now()
+                            f.write(writeable)
+                            end = datetime.now()
+                    except PermissionError:
+                        if us:
+                            logging.error("permission denied")
+                        sys.exit(2)
+                else:
                     start = datetime.now()
-                    f.write(writeable)
-                    end = datetime.now()
-            except PermissionError:
-                if us:
-                    logging.error("permission denied")
-                sys.exit(2)
-        else:
-            start = datetime.now()
-            try:
-                with open(assembledname,"w+") as f:
-                    f.write(writeable)           
-            except PermissionError:
-                logging.error("permission denied")
-                sys.exit(2)
-            end = datetime.now() 
-        speed = end - start
-        speed = speed.total_seconds()*1000
-        dspeed = 1000 / speed
-        dspeed = dspeed / diffval
-        speeds.append(speed)
-        dspeeds.append(dspeed)
-        if "--noround" in args:
-            logging.info(f"Speed is {dspeed} MB/s")
-            logging.info(f"Wrote {wsize/1000} KB to the disk in {speed} milliseconds.")
-        else:
-            logging.info(f"Speed is {round(dspeed,3)} MB/s")
-            logging.info(f"Wrote {wsize/1000} KB to the disk in {round(speed,3)} milliseconds.")
+                    try:
+                        with open(assembledname,"w+") as f:
+                            f.write(writeable)           
+                    except PermissionError:
+                        logging.error("permission denied")
+                        sys.exit(2)
+                    end = datetime.now() 
+                speed = end - start
+                speed = speed.total_seconds()*1000
+                speed = round(speed,3)
+                dspeed = 1000 / speed
+                dspeed = dspeed / diffval
+                speeds.append(speed)
+                dspeeds.append(dspeed)
+                if "--noround" in args:
+                    logging.info(f"Speed is {dspeed} MB/s")
+                    logging.info(f"Wrote {wsize/1000} KB to the disk in {speed} milliseconds.")
+                else:
+                    logging.info(f"Speed is {round(dspeed,3)} MB/s")
+                    logging.info(f"Wrote {wsize/1000} KB to the disk in {round(speed,3)} milliseconds.")
+                success = True
+            except ZeroDivisionError:
+                logging.warning(f"Invalid test {i+1}. Redoing")
     logging.info("All tests done")
     print(f"Average speed is {round(average(dspeeds),3)} Mb/s")
     print(f"Average time is {round(average(speeds),3)} ms")
-    print(f"Lowest time was {min(speeds)} ms ({max(dspeeds)} Mb/s)")
+    print(f"Fastest time was {min(speeds)} ms ({max(dspeeds)} Mb/s)")
     print(f"Slowest time was {max(speeds)} ms ({min(dspeeds)} Mb/s)")
     if not "--noremove" in args:
         logging.info("Removing file")
         os.remove(assembledname)
 else:
-    if "--excludefmk" in args or "-e" in args:
-        logging.info("Excluding file make")
+    success = False
+    while not success:
         try:
-            with open(assembledname,"w+") as f:
+            if "--excludefmk" in args or "-e" in args:
+                logging.info("Excluding file make")
+                try:
+                    with open(assembledname,"w+") as f:
+                        start = datetime.now()
+                        f.write(writeable)
+                        end = datetime.now()
+                except PermissionError:
+                    logging.error("Permission denied")
+                    sys.exit(2)
+            else:
                 start = datetime.now()
-                f.write(writeable)
+                try:
+                    with open(assembledname,"w+") as f:
+                        f.write(writeable)           
+                except PermissionError:
+                    logging.error("permission denied")
+                    sys.exit(2)
                 end = datetime.now()
-        except PermissionError:
-            logging.error("Permission denied")
-            sys.exit(2)
-    else:
-        start = datetime.now()
-        try:
-            with open(assembledname,"w+") as f:
-                f.write(writeable)           
-        except PermissionError:
-            logging.error("permission denied")
-            sys.exit(2)
-        end = datetime.now()
-    if not "--noremove" in args:
-        logging.info("Removing file")
-        os.remove(assembledname)
-    logging.info("Calculating")
-    speed = end - start
-    speed = speed.total_seconds()*1000
-    dspeed = 1000 / speed
-    dspeed = dspeed / diffval
-    logging.info("Printing results")
-    if "--noround" in args:
-        print(f"Speed is {dspeed} MB/s")
-        print(f"Wrote {wsize/1000} KB to the disk in {speed} milliseconds.")
-    else:
-        print(f"Speed is {round(dspeed,3)} MB/s")
-        print(f"Wrote {wsize/1000} KB to the disk in {round(speed,3)} milliseconds.")
+            if not "--noremove" in args:
+                logging.info("Removing file")
+                os.remove(assembledname)
+            logging.info("Calculating")
+            speed = end - start
+            speed = speed.total_seconds()*1000
+            speed = round(speed,3)
+            dspeed = 1000 / speed
+            dspeed = dspeed / diffval
+            logging.info("Printing results")
+            if "--noround" in args:
+                print(f"Speed is {dspeed} MB/s")
+                print(f"Wrote {wsize/1000} KB to the disk in {speed} milliseconds.")
+            else:
+                print(f"Speed is {round(dspeed,3)} MB/s")
+                print(f"Wrote {wsize/1000} KB to the disk in {round(speed,3)} milliseconds.")
+            success = True
+        except ZeroDivisionError:
+            logging.warning("Test invalid. Retrying")
 logging.info("Done")
 sys.exit(0)
